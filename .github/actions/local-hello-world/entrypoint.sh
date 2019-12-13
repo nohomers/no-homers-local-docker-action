77b2dace416e78f2aa61b5580f70a2287fe710ce
Lucas Costi <lucascosti@github.com>
2019-12-13 12:03:17 +1000

    test for env

--- .github/actions/local-hello-world/entrypoint.sh
+++ .github/actions/local-hello-world/entrypoint.sh
@@ -5,6 +5,8 @@ time=$(date)
 
 echo ::set-output name=time::$time
 
+echo "The Simpson's house address is: $address"
+
 if echo $name | grep -i "^homer" > /dev/null ; then
   echo "No Homers allowed!"
   # Exit with a failure
