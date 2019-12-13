99d1a61007a6857a8b4db2b90c310381378b3014
Lucas Costi <lucascosti@github.com>
2019-12-13 12:10:18 +1000

    go

--- .github/actions/local-hello-world/entrypoint.sh
+++ .github/actions/local-hello-world/entrypoint.sh
@@ -5,7 +5,7 @@ time=$(date)
 
 echo ::set-output name=time::$time
 
-echo "The Simpson's house address is: $address"
+echo "The Simpson's house address is: " $home_address
 
 if echo $name | grep -i "^homer" > /dev/null ; then
   echo "No Homers allowed!"
