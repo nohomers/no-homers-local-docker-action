8f1c207c7e5cccc49952f3892ae0409b15a7a0c7
Lucas Costi <lucascosti@users.noreply.github.com>
2020-03-18 14:45:14 +1000

    Add quotes for testing #12792

--- .github/actions/local-hello-world/entrypoint.sh
+++ .github/actions/local-hello-world/entrypoint.sh
@@ -3,7 +3,7 @@
 name="$1"
 time=$(date)
 
-echo ::set-output name=time::$time
+echo "::set-output name=time::$time"
 
 if echo $name | grep -i "^homer" > /dev/null ; then
   echo "No Homers allowed!"
