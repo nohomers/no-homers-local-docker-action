27d3898638d7e829383ebf041a52c63850440865
Lucas Costi <lucascosti@users.noreply.github.com>
2020-05-14 13:32:57 +1000

    debug log test

--- .github/actions/local-hello-world/entrypoint.sh
+++ .github/actions/local-hello-world/entrypoint.sh
@@ -3,6 +3,8 @@
 name="$1"
 time=$(date)
 
+echo "::debug file=entrypoint.sh,line=4::This was the time set step"
+
 echo "::set-output name=time::$time"
 
 if echo $name | grep -i "^homer" > /dev/null ; then
@@ -12,4 +14,3 @@ if echo $name | grep -i "^homer" > /dev/null ; then
 else
   echo "Hello $name"
 fi
-
